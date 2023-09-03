9f50b8efdce4f5b4d1fae312c08490a619b2f274
ceimiplace <azamfiredenis@yahoo.com>
2023-09-03 10:21:20 +0300

    Added state and 2 way data binding

--- src/components/InfoAndEdit.js
+++ src/components/InfoAndEdit.js
@@ -1,26 +1,37 @@
-import React from "react";
-const InfoAndEdit = () =>{
-    return   <div className='containerInfo'>
+import React, { useState } from "react";
+const InfoAndEdit = ({data}) =>{
+    const [editing,setEditing] = useState(false);
+   
+    const [title,setTitle] = useState("");
+    const [firstName,setFirstName] = useState("");
+    const [lastName,setLastName] = useState("");
+    const [dateOfBirth,setDateOfBirth] = useState("");
+    return <div className='containerInfo'>
+    <div className="editContainer">
     <div>
-    <label for="title">Title</label>
-    <select name="title" id="title">
+    <label htmlFor="title">Title</label>
+    <select name="title" id="title" onChange={(elem)=>{setTitle(elem.target.value)}} value={title}>
       <option>Mr.</option>
       <option>Mrs.</option>
       <option>Miss</option>
     </select>
     </div>
     <div>
-    <label for="firstName" >First Name</label>
-    <input type="text" required  id="firstName" />
+    <label htmlFor="firstName" >First Name</label>
+    <input type="text" required  id="firstName" onChange={(elem)=>setFirstName(elem.target.value)} value={firstName}/>
     </div>
     <div>
-    <label for="lastName">Last Name</label>
-    <input type="text" required id="lastName"/>
+    <label htmlFor="lastName">Last Name</label>
+    <input type="text" required id="lastName" onChange={(elem)=>setLastName(elem.target.value)} value={lastName}/>
     </div>
     <div>
-    <label for="dateOfBirth">Date of bith</label>
-    <input type="date" required id="dateOfBirth"/>
+    <label htmlFor="dateOfBirth">Date of bith</label>
+    <input type="date" required id="dateOfBirth" onChange={(elem)=>setDateOfBirth(elem.target.value)} value={dateOfBirth}/>
     </div>
+    </div>
+    <div className="detailsContainer"></div>
+    <button onClick={()=>setEditing(prevState=>{console.log("clicked"); return !prevState})}>Edit</button>
+    
   </div>
 }
 export default InfoAndEdit;
